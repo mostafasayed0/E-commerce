5e1a94e2c0e4175505954caad15f21ba59cf3848
mostafasayed <mostafa.sayed753951@gmail.com>
2025-06-16 04:54:24 +0300

    add show pass

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,60 +9,55 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule , RouterLink ,TranslateModule],
+  imports: [ReactiveFormsModule, RouterLink, TranslateModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 // export class LoginComponent {
 // loginform:FormGroup= new FormGroup({
 //   email:new FormControl(null , [Validators.required , Validators.email]),
 //   password:new FormControl(null , [Validators.required,Validators.pattern(/^\w{8,}[@#$]/)])
 // })
-
 export class LoginComponent implements OnDestroy {
-  isLoading:boolean=false;
-  SubscripeValue!:Subscription;
+  isLoading: boolean = false;
+  SubscripeValue!: Subscription;
+  showPassword: boolean = false;
 
+  private readonly _FormBuilder = inject(FormBuilder);
+  private readonly _AuthService = inject(AuthService);
+  private readonly _Router = inject(Router);
+  loginform: FormGroup = this._FormBuilder.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.pattern(/^\w{6,}[@#$]/)]],
+  });
 
-  private readonly _FormBuilder=inject(FormBuilder)
-  private readonly _AuthService=inject(AuthService)
-  private readonly _Router=inject(Router)
-loginform:FormGroup= this._FormBuilder.group({
-  email:[null , [Validators.required , Validators.email]],
-  password:[null , [Validators.required,Validators.pattern(/^\w{6,}[@#$]/)]]
-})
-
-send()
-{
-  this.isLoading=true;
-if(this.loginform.valid)
-{
- this.SubscripeValue= this._AuthService.SetLogin(this.loginform.value).subscribe({
-next:(res)=>{
-if(res.message=='success')
-{
-  setTimeout(() => {
-    localStorage.setItem('userToken', res.token)
-    this._AuthService.saveUserData();
-    this._Router.navigate(['/home'])
-  }, 1000);
-}
-},
-error:(err:HttpErrorResponse)=> {
-  this.isLoading=false;
-},
-
-})
-}
-else
-{
-  this.loginform.markAllAsTouched()
-  this.isLoading=false;
-}
-}
-ngOnDestroy(): void {
-  this.SubscripeValue?.unsubscribe()
-}
+  send() {
+    this.isLoading = true;
+    if (this.loginform.valid) {
+      this.SubscripeValue = this._AuthService
+        .SetLogin(this.loginform.value)
+        .subscribe({
+          next: (res) => {
+            if (res.message == 'success') {
+              setTimeout(() => {
+                localStorage.setItem('userToken', res.token);
+                this._AuthService.saveUserData();
+                this._Router.navigate(['/home']);
+              }, 1000);
+            }
+          },
+          error: (err: HttpErrorResponse) => {
+            this.isLoading = false;
+          },
+        });
+    } else {
+      this.loginform.markAllAsTouched();
+      this.isLoading = false;
+    }
+  }
+  ngOnDestroy(): void {
+    this.SubscripeValue?.unsubscribe();
+  }
 }
 
 
